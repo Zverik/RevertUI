c4958184ca6eadab6f30124052bd9069f47d4d1b
Ilya Zverev <zverik@textual.ru>
2016-06-23 00:22:54 +0300

    Path to venv for revert_job.py

--- revertui/revert_job.py
+++ revertui/revert_job.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 import sys
 import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, BASE_DIR)
+
+PYTHON = 'python2.7'
+VENV_DIR = os.path.join(BASE_DIR, 'venv', 'lib', PYTHON, 'site-packages')
+if os.path.exists(VENV_DIR):
+    sys.path.insert(1, VENV_DIR)
+
 import config
 import requests
 import atexit
