b185a1a5b8ebfda640abba8025e6e3cc721c8513
Ilya Zverev <zverik@textual.ru>
2016-06-22 11:47:31 +0300

    fix

--- revertui/config.py
+++ revertui/config.py
@@ -2,8 +2,9 @@ DEBUG = True
 
 CREATED_BY = 'RevertUI 1.0'
 
-OAUTH_KEY = 'hB4vjO7nXlF6lBdZbITsFmAjKEywIX9iH8uBYrcK'
-OAUTH_SECRET = 'BITjalQOm78lwEaq50LAh0vHZzJCn7QEuw9b5QYz'
+# Get these at osm.org -> account -> oauth settings -> register
+OAUTH_KEY = ''
+OAUTH_SECRET = ''
 
 MAX_CHANGESETS = 20
 MAX_DIFFS = 200
@@ -12,4 +13,5 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE_PATH = os.path.join(BASE_DIR, 'revertui.db')
 
+# Change this for the production
 SECRET_KEY = 'sakdfjhasldfhasldfsdfas21421432134'
